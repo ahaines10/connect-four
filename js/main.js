88c10f515cd43ed80e6bcb5fdae8783324aedd87
username <youremail@domain.com>
2022-07-05 16:26:58 -0700

    adds rendering

--- js/main.js
+++ js/main.js
@@ -1,17 +1,56 @@
 /*----- constants -----*/
-// const player1;
-// const player2;
-// const board;
-let board = document.getElementById("board");
-
+const PLAYERS_COLORS = {
+  1: "green",
+  "-1": "blue",
+  0: "white",
+};
 /*----- app's state (variables) -----*/
+let board;
+let winner; // null = game in play    1/-1 has won 'T'
+let turn; //  1/-1
+let gameStatus;
+
 /*----- cached element references -----*/
+const boardEl = document.getElementById("board");
 /*----- event listeners -----*/
-document.getElementById("board").addEventListener(function().onclick) ;
-console.log(board);
+boardEl.addEventListener("click", handleMove);
 
 /*----- functions -----*/
 //if player clicks on row 1 player will have token of red
 //if player 2 click on row player 2 will have token of blue
 //players click in a array if player gets 4 in a row player wins
 //if player clicks 3 or less  player dosnt win;
+init();
+function init() {
+  board = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+  ];
+
+  turn = 1;
+  gameStatus = null;
+  render();
+}
+function render() {
+  board.forEach(function (colArr, colIdx) {
+    colArr.forEach(function (cellVal, rowIdx) {
+      //select the div for colidx and rowidx
+      let currPos = document.getElementById(`c${colIdx}r${rowIdx}`);
+      console.log(currPos, colIdx, rowIdx);
+      currPos.style.backgroundColor = PLAYERS_COLORS[cellVal];
+    });
+  });
+}
+function handleMove(evt) {
+  console.log(evt.target, "handleMove");
+  let col = evt.target.id[1];
+  let row = evt.target.id[3];
+  board[col][row] = turn;
+  turn = turn * -1;
+  render();
+}
