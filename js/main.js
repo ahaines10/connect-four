9c806cb24ac81efe5dc496f27e9df44f7e46755b
username <youremail@domain.com>
2022-07-05 09:47:41 -0700

    add index html js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 /*----- constants -----*/
+// const player1;
+// const player2;
+// const board;
+let board = document.getElementById("board");
+
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
 /*----- event listeners -----*/
+document.getElementById("board").addEventListener(function().onclick) ;
+console.log(board);
+
 /*----- functions -----*/
+//if player clicks on row 1 player will have token of red
+//if player 2 click on row player 2 will have token of blue
+//players click in a array if player gets 4 in a row player wins
+//if player clicks 3 or less  player dosnt win;
